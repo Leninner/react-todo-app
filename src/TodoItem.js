4bc774d6fc810fa634b87881902554d70caff2e0
leninner <mazabandalenin180@gmail.com>
2021-08-23 15:51:53 -0500

    Añado diseño

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -4,7 +4,7 @@ import './TodoItem.css';
 function TodoItem(props) {
   return (
     <li className='TodoItem'>
-      <span className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}>√</span>
+      <span className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}>ᄼ</span>
       <p className={`TodoItem-p ${props.completed && 'TodoItem-p--complete'}`}>{props.text}</p>
       <span className='Icon Icon-delete'>X</span>
     </li>
