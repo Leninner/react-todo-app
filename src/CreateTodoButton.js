13710700c0b20e3dff7eacc91e2fe777b09049a1
leninner <mazabandalenin180@gmail.com>
2021-08-23 13:36:08 -0500

    Añado archivos de estilos para TODO app

--- src/CreateTodoButton.js
+++ src/CreateTodoButton.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import './CreateTodoButton.css';
 
 function CreateTodoButton() {
-  return <button>+</button>;
+  return <button className='CreateTodoButton'>+</button>;
 }
 
 export { CreateTodoButton };
