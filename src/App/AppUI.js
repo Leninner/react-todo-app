f798d284892d0541e51ec6c507889aa88b8621df
leninner <mazabandalenin180@gmail.com>
2021-08-26 15:38:17 -0500

    Añado el estado para loading y errores

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -6,13 +6,27 @@ import { TodoItem } from '../TodoItem';
 import { TodoList } from '../TodoList';
 import { TodoBrand } from '../TodoBrand';
 
-function AppUI({ totalTodos, completedTodos, searchValue, setSearchValue, searchedTodos, completeTodos, deleteTodos }) {
+function AppUI({
+  loading,
+  error,
+  totalTodos,
+  completedTodos,
+  searchValue,
+  setSearchValue,
+  searchedTodos,
+  completeTodos,
+  deleteTodos,
+}) {
   return (
     <React.Fragment>
       <TodoBrand />
       <TodoCounter total={totalTodos} completed={completedTodos} />
       <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
       <TodoList>
+        {error && <p>Desespérate, hubo un error</p>}
+        {loading && <p>Estamos cargando, no desesperes</p>}
+        {!loading && !searchedTodos.length && <p>Crea tu primer TODO...</p>}
+
         {searchedTodos.map((todo) => (
           <TodoItem
             key={todo.text}
--- src/App/index.js
+++ src/App/index.js
@@ -10,31 +10,48 @@ import { AppUI } from './AppUI';
 
 //REACT HOOk
 function useLocalStorage(itemName, initialValue) {
-  //NOTE: Local Storage
-  const localStorageItem = localStorage.getItem(itemName);
-  let parsedItem;
+  const [error, setError] = React.useState(false);
+  const [loading, setLoading] = React.useState(true);
 
-  //Lógica para comprobar si ya han usado la aplicación o no.
-  if (!localStorageItem) {
-    localStorage.setItem(itemName, JSON.stringify(initialValue));
-    parsedItem = initialValue;
-  } else {
-    parsedItem = JSON.parse(localStorageItem);
-  }
+  const [item, setItem] = React.useState(initialValue);
 
-  const [item, setItem] = React.useState(parsedItem);
+  React.useEffect(() => {
+    setTimeout(() => {
+      try {
+        // Nos ayuda a ejecutar código pero si ocurre un error, utilizamos catch y no se crashee
+        const localStorageItem = localStorage.getItem(itemName);
+        let parsedItem;
+
+        //Lógica para comprobar si ya han usado la aplicación o no.
+        if (!localStorageItem) {
+          localStorage.setItem(itemName, JSON.stringify(initialValue));
+          parsedItem = initialValue;
+        } else {
+          parsedItem = JSON.parse(localStorageItem);
+        }
+        setItem(parsedItem);
+        setLoading(false);
+      } catch (error) {
+        setError(error);
+      }
+    }, 1000);
+  });
 
   //Función para controlar el flujo de la app
   const saveItem = (newItem) => {
-    const stringifiedItem = JSON.stringify(newItem);
-    localStorage.setItem(itemName, stringifiedItem);
-    setItem(newItem);
+    try {
+      const stringifiedItem = JSON.stringify(newItem);
+      localStorage.setItem(itemName, stringifiedItem);
+      setItem(newItem);
+    } catch (error) {
+      setError(error);
+    }
   };
-  return [item, saveItem];
+  return { item, saveItem, loading, error };
 }
 
 function App() {
-  const [todos, saveTodos] = useLocalStorage('TODOS_V1', []);
+  const { item: todos, saveItem: saveTodos, loading, error } = useLocalStorage('TODOS_V1', []);
 
   const [searchValue, setSearchValue] = React.useState(''); // Forma de trabajar los estados.
 
@@ -81,6 +98,8 @@ function App() {
 
   return (
     <AppUI
+      loading={loading}
+      error={error}
       totalTodos={totalTodos}
       completedTodos={completedTodos}
       searchValue={searchValue}
